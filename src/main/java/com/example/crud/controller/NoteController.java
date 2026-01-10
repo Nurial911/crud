df84238049911b00e1e04bc92a705396d17116b3
Nurial <nurali.manarbekov@nu.edu.kz>
2026-01-10 08:55:40 +0500

    feat: add get methods

--- src/main/java/com/example/crud/controller/NoteController.java
+++ src/main/java/com/example/crud/controller/NoteController.java
@@ -1,9 +1,15 @@
 package com.example.crud.controller;
 
+import com.example.crud.dto.CreateNoteRequest;
+import com.example.crud.dto.NoteResponse;
+import com.example.crud.entities.Note;
 import com.example.crud.service.NoteService;
 import lombok.Data;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -11,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
     private final NoteService noteService;
 
+    @GetMapping("/allnotes")
+    public Iterable<NoteResponse> getAllNotes(){
+        return noteService.getAllNotes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NoteResponse> getNoteById(@PathVariable Long id){
+        return noteService.getNoteById(id);
+    }
 }
--- src/main/java/com/example/crud/service/NoteService.java
+++ src/main/java/com/example/crud/service/NoteService.java
@@ -1,13 +1,37 @@
 package com.example.crud.service;
 
+import com.example.crud.dto.NoteResponse;
+import com.example.crud.entities.Note;
+import com.example.crud.mappers.NoteMapper;
 import com.example.crud.repositories.NoteRepository;
 import lombok.Data;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 @Data
 public class NoteService {
     private final NoteRepository noteRepository;
+    private final NoteMapper noteMapper;
+
+    public Iterable<NoteResponse> getAllNotes(){
+        return noteRepository.findAll()
+                .stream()
+                .map(noteMapper::toResponse)
+                .toList();
+    }
+
+    public ResponseEntity<NoteResponse> getNoteById(Long id){
+        var note = noteRepository.findById(id).orElse(null);
+        if(note == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(noteMapper.toResponse(note));
+    }
+
 
 
 }
